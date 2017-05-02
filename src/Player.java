c227bd1230bfc42adaf4bdebd71c4fda77511689
Corey Jones <coreyjones@Heathers-MacBook-Pro-2.local>
2017-05-01 19:47:44 -1000

    Initial commit

---
+++ src/Player.java
@@ -0,0 +1,16 @@
+/**
+ * Created by coreyjones on 5/1/17.
+ */
+public class Player
+{
+    //Instance Variables
+    private String playName;
+    private boolean Warr;
+
+
+    //Character Constructor
+    public void Player(String initName)
+    {
+        playName = initName;
+    }
+}
